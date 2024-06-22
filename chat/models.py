975a2206a943217b3ea270c294a3ebcd762fe582
Sarthak <sarthakrangari788@gmail.com>
2024-06-22 21:57:37 +0530

    get_user_model in chat.models

--- chat/models.py
+++ chat/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 
-User = get_user_model()
+
 
 # Create your models here.
 
@@ -15,8 +15,8 @@ class ThreadManager(models.Manager):
 
 
 class Thread(models.Model):
-    first_person = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='thread_first_person')
-    second_person = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True,
+    first_person = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True, related_name='thread_first_person')
+    second_person = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True,
                                      related_name='thread_second_person')
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -28,6 +28,6 @@ class Thread(models.Model):
 
 class ChatMessage(models.Model):
     thread = models.ForeignKey(Thread, null=True, blank=True, on_delete=models.CASCADE, related_name='chatmessage_thread')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
