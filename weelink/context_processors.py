b3567d6c0808982bd4f82a61753899d3abf39c38
Sarthak <sarthakrangari788@gmail.com>
2024-06-22 20:51:51 +0530

    postgres setup

--- weelink/context_processors.py
+++ weelink/context_processors.py
@@ -7,6 +7,15 @@ def base_template_data(request):
 
         threads = Thread.objects.by_user(user=request.user).prefetch_related('chatmessage_thread').order_by('timestamp')
         
+
+        if not threads.exists():
+            context = {
+                'threads': [],
+                'latest_thread_id': 0
+            }
+            return context
+
+
         for thread in threads:
             latest_message = thread.chatmessage_thread.order_by('-timestamp').first()
             if latest_message:
--- weelink/settings.py
+++ weelink/settings.py
@@ -83,10 +83,24 @@ ASGI_APPLICATION = 'weelink.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+# Development database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Deployment database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'OAyuPMgOPOEtUtqsgXKjFNbRQFdVZMTz',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '56979',
     }
 }
 
